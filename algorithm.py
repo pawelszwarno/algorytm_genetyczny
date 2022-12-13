e93207ba540fbc4744d89f0ab449e20691e5458e
sztefkomateusz <sztefkomateusz@gmail.com>
2022-12-14 00:23:03 +0100

    ok

--- algorithm.py
+++ algorithm.py
@@ -1,4 +1,4 @@
-from classes import Order, Truck, SolutionTuple, Graph
+from classes import Order, Truck, SolutionTuple, Graph, TruckType
 from typing import List
 from random import randint
 from random import choice
@@ -75,12 +75,35 @@ def crossing(parent_1: List[List[SolutionTuple]], parent_2: List[List[SolutionTu
     return new_sol
 
 
+# stworzenie grafu:
+g = Graph(6, 6)
+g.create_adj_matrix(0, 10)
+# print("Macierz sąsiedztwa: ")
+# print(g, '\n')
+
+# stworzenie listy ciężarówek:
+n_small_trucks = 7
+n_large_trucks = 6
+trucks_list = [Truck(TruckType.SMALL) for _ in range(n_small_trucks)]
+for _ in range(n_large_trucks):
+    trucks_list.append(Truck(TruckType.LARGE))
+# print("Lista ciężarówek: (Index, typ)")
+# print(trucks_list, '\n')
+
+# Stworzenie listy zleceń złożonej z instancji klasy Order z polami:
+# deadline - czyli czas w którym trzeba dostarczyć dostawę i * 1000 (wyrażany w minutach np.)
+orders_lst = [Order(g, 20) for i in range(20)]
+# print("Lista zleceń: ")
+# print(orders_lst, '\n')
+# lista zleceń, liczba dużych ciężarówek, liczba małych ciężarówek,
+# pojemność małych ciężarówek, pojemność dużych ciężarówek
+
 def algorithm(n_population: int, n_iteration: int , r_cross: float, r_mutation: float):
     children = []
-    population = generate_solution()
-    best, best_eval = 0, objective_function(population[0])
+    population = generate_solution(trucks_list, orders_lst, n_large_trucks, n_small_trucks)
+    best, best_eval = 0, objective_function(population[0], g, trucks_list, orders_lst)
     for generation in range(n_iteration):
-        scores = [objective_function(c) for c in population]
+        scores = [objective_function(child, g, trucks_list, orders_lst) for child in population]
         for i in range(n_population):
             if scores[i] < best_eval:
                 best, best_eval = population[i], scores[i]
@@ -93,6 +116,26 @@ def algorithm(n_population: int, n_iteration: int , r_cross: float, r_mutation: 
         population = children
     return best, best_eval
 
-def selection_rank(population, scores):
+def selection_rank(population, scores, n_population):
+    sel_lst = []
+    
+    for ix in range(len(population)):
+        if len(sel_lst) == 0:
+            sel_lst.append((population[ix],scores[ix]))
 
-    pass
+        if scores[ix] > sel_lst[-1][1]:
+            if len(sel_lst) >= n_population:
+                sel_lst[-1] = (population[ix],scores[ix])
+            elif len(sel_lst) < n_population:
+                sel_lst.append((population[ix],scores[ix]))
+        sel_lst.sort(reverse = 1, key = lambda x: x[1])
+    
+    return sel_lst
+
+# popi = [1,2,3,4,5,6,((1,3),(2,1),(5,1),(6,2),(8,4)),8,9,10,12,16]
+# scor = [1,2,3,4,5,6,7,8,9,10,12,16]
+# lens = 10
+# print(selection_rank(popi, scor, lens))
+
+# print(generate_solution(trucks_list, orders_lst, n_large_trucks, n_small_trucks))
+print(algorithm(n_population, n_iteration, r_cross, r_mutation))
