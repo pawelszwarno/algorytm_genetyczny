2706ec55ce8e678105d21c3b8fc8a5806e3dd794
Piotr Suchy <piotrsuchy@Piotrs-MacBook-Pro.local>
2022-12-14 12:53:51 +0100

    dodane create_structure

--- algorithm.py
+++ algorithm.py
@@ -1,8 +1,7 @@
-from classes import Order, Truck, SolutionTuple, Graph
+from classes import Order, Truck, SolutionTuple, Graph, TruckType
 from typing import List
 from random import randint, choice, shuffle, random
-from variables import penalty_factor
-
+from variables import penalty_factor, n_small_trucks, n_large_trucks
 
 # TO DO
 def generate_solution(trucks_list: List[Truck], orders_list: List[Order], n_large_truck: int, n_small_truck: int) -> List[List[SolutionTuple]]:
@@ -51,6 +50,7 @@ def objective_function(solution: List[List[SolutionTuple]], cost_graph: Graph, t
                 prev_order = curr_order
     return cost
 
+
 # funkcja mutacji wariant 1: zamiana kolejności w trasie losowej ilości ciężarówek 
 def mutation(new_sol: List[List[SolutionTuple]], truck_list: List[Truck], r_mut = 1.0):
     rand_float = random()
@@ -73,8 +73,6 @@ def mutation(new_sol: List[List[SolutionTuple]], truck_list: List[Truck], r_mut 
             new_sol[truck_id_to_mut][right_swap] = temp
         return new_sol
             
-            
-            
 
 def crossing(parent_1: List[List[SolutionTuple]], parent_2: List[List[SolutionTuple]], r_cross: int):
     new_sol = [[] for _ in range(len(parent_1))]
@@ -94,5 +92,62 @@ def crossing(parent_1: List[List[SolutionTuple]], parent_2: List[List[SolutionTu
     return new_sol
 
 
-def algorithm():
-    pass
+def selection_rank(population, scores, n_population):
+    sel_lst = []
+    
+    for ix in range(len(population)):
+        if len(sel_lst) == 0:
+            sel_lst.append((population[ix],scores[ix]))
+
+        if scores[ix] > sel_lst[-1][1]:
+            if len(sel_lst) >= n_population:
+                sel_lst[-1] = (population[ix],scores[ix])
+            elif len(sel_lst) < n_population:
+                sel_lst.append((population[ix],scores[ix]))
+        sel_lst.sort(reverse = 1, key = lambda x: x[1])
+    
+    return sel_lst
+
+def create_structures(rows_cols: int, low_adj_matrix, high_adj_matrix: float, n_of_orders: int):
+    # stworzenie grafu:
+    g = Graph(rows_cols, rows_cols)
+    g.create_adj_matrix(low_adj_matrix, high_adj_matrix)
+    # print("Macierz sąsiedztwa: ")
+    # print(g, '\n')
+    
+    # stworzenie listy ciężarówek:
+    trucks_list = [Truck(TruckType.SMALL) for _ in range(n_small_trucks)]
+    for _ in range(n_large_trucks):
+        trucks_list.append(Truck(TruckType.LARGE))
+    # print("Lista ciężarówek: (Index, typ)")
+    # print(trucks_list, '\n')
+    
+    # Stworzenie listy zleceń złożonej z instancji klasy Order z polami:
+    # deadline - czyli czas w którym trzeba dostarczyć dostawę i * 1000 (wyrażany w minutach np.)
+    orders_lst = [Order(g, 20) for _ in range(n_of_orders)]
+    # print("Lista zleceń: ")
+    # print(orders_lst, '\n')
+    # lista zleceń, liczba dużych ciężarówek, liczba małych ciężarówek,
+    # pojemność małych ciężarówek, pojemność dużych ciężarówek
+    return g, trucks_list, orders_lst
+    
+    
+def algorithm(n_population: int, n_iteration: int , r_cross: float, r_mutation: float, truck_list: List[Truck], order_lst: List[Order], g: Graph):
+    children = []
+    population = generate_solution(truck_list, order_lst, n_large_trucks, n_small_trucks)
+    best, best_eval = population, objective_function(population, g, truck_list, order_lst)
+    for generation in range(n_iteration):
+        scores = [objective_function(child, g, truck_list, order_lst) for child in population]
+        for i in range(n_population):
+            if scores[i] < best_eval:
+                best, best_eval = population[i], scores[i]
+        selected = selection_rank(population, scores)
+        for i in range(0, n_population, 2):
+            parent_1, parent_2 = selected[i], selected[i+1]
+            for child in crossing(parent_1, parent_2, r_cross):
+                mutation(child, r_mutation)
+                children.append(child)
+        population = children
+    return best, best_eval
+
+
