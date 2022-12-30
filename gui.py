865c90b52ee7ce682cc5540b46ca9e76f69e9547
Pawel Szwarnowski <pawel.szwarnowski@o2.pl>
2022-12-30 19:57:12 +0100

    Improved data validation in gui

--- gui.py
+++ gui.py
@@ -8,6 +8,7 @@ window = tk.Tk()
 window.title("Variable Setter")
 
 var_names = ["SIMULATION_TIME", "speed_l", "capacity_l", "speed_s", "capacity_s", "n_small_trucks", "n_large_trucks", "n_pop", "n_iteration", "penalty_factor", "r_cross", "r_mutation", "parent_percent"]
+integer_vars = ["SIMULATION_TIME", "capacity_l", "capacity_s", "n_small_trucks", "n_large_trucks", "n_pop", "n_iteration", "r_cross"]
 # Create a function to run the other functions that use the variables
 def run_functions():
     # Save the variables to a dictionary
@@ -21,20 +22,72 @@ def run_functions():
 # Create a list to hold the variables
 variables = []
 
+def validate_data(checked_value, var_name):
+    if len(checked_value) == 0:
+        return f'{var_name} should be filled'
+    if var_name in integer_vars:
+        try:
+            int_value = int(checked_value)
+            if int_value - float(checked_value) != 0:
+                return f'{var_name} should be integer'
+            if var_name == 'n_small_trucks':
+                if int_value < 0:
+                    return f'{var_name} should be non-negative'
+            elif var_name == 'n_large_trucks':
+                if int_value < 0:
+                    return f'{var_name} should be non-negative'
+                if int_value + var_names['n_small_trucks'] == 0:
+                    return f'There should be at least one truck'
+            elif var_name == 'r_cross':
+                if int_value < 0:
+                    return f'{var_name} should be non-negative'
+            else:
+                if int_value <= 0:
+                    return f'{var_name} should be positive'
+            
+        except ValueError:
+            return f'{var_name} should be integer'
+        
+        else:
+            variables.append(int_value)
+            return 'Success'
+     
+    else:
+        try:
+            float_value = float(checked_value) # raises ValueError when conversion is not possible
+            
+            if var_name == 'r_mutation':
+                if float_value > 1:
+                    return f'r_mutation should be lower or equal to 1'
+                if float_value < 0:
+                    return f'r_mutation should be higher or equal to 0'
+            elif var_name == 'parent_percent':
+                if float_value > 100:
+                    return f'parent_percent should be lower or equal to 100'
+                if float_value < 0:
+                    return f'r_mutation should be higher or equal to 0'                
+            else: 
+                if float_value <= 0:
+                    return f'{var_name} should be positive'
+        
+        except ValueError:
+            return f'{var_name} should be float'
+        
+        else:
+            variables.append(float_value)
+            return 'Success'
+            
+    
+
 # Create a function to get the values from the entries
 def get_values():
     global variables
     variables = []
     try:
-        for entry in entries:
+        for idx, entry in enumerate(entries):
             checked_value = entry.get()
-            if len(checked_value) == 0:
-                msg = 'All entries have to be filled!'
-                raise ValueError
-            if checked_value.isnumeric():
-                variables.append(checked_value)
-            else:
-                msg = 'Only positive integers are accepted!'
+            msg = validate_data(checked_value, var_name=var_names[idx])
+            if msg != 'Success':
                 raise ValueError
     except ValueError:
         messagebox.showerror('Value Error', msg)
