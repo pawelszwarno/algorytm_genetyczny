59427eb6608c5dde4019514c49fc13ca3fe81146
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-01-03 01:25:11 +0100

    W/e

--- tests.py
+++ tests.py
@@ -1,9 +1,13 @@
-from algorithm import selection_prop, selection_tour, selection
+# from src/algorithm import selection_prop, selection_tour, selection
+from main import main
 
+#TODO: testowanie selekcji prop i tour:
 population_scores = [(0, 100), (1, 90), (2,120), (3,160), (4, 100), (5, 90), (6,120), (7,160), (8, 100), (9, 90), (10,120), (11,160)] 
 npop = 4
 
-print(selection_tour(population_scores, npop))
+# output = main()
+
+# print(output)
 # print(selection(population_scores, npop))
 # print(selection_prop(a, npop))
 
