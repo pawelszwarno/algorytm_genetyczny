37f0cee7392baa6c7f82cd900cb334315bd2d938
Piotr Suchy <piotrsuchy6@tlen.pl>
2023-01-03 01:24:13 +0100

    Zmiana z printów na jeden output, aby łatwiej było używać potem w gui.py

--- main.py
+++ main.py
@@ -5,16 +5,29 @@ def main():
     import src.algorithm as algorithm
     from src.classes import Order, Truck, TruckType, Graph
     from src.algorithm import visualise
-    
+    from io import StringIO
+    import sys
+
     cwd = Path().cwd()
     json_path = cwd / 'data' / 'variables.json'
     with open(json_path) as f:
         variables = json.load(f)
-    
-    g, trucks_list, orders_lst = algorithm.create_structures(rows_cols=6, low_adj_matrix=0, high_adj_matrix=10, n_of_orders=40)
-    
+
+    g, trucks_list, orders_lst = algorithm.create_structures(
+        rows_cols=6, low_adj_matrix=0, high_adj_matrix=10, n_of_orders=40)
+
+    # Save the current stdout
+    original_stdout = sys.stdout
+
+    # Create a buffer to hold the output
+    output_buffer = StringIO()
+
+    # Redirect the output to the buffer
+    sys.stdout = output_buffer
+
     selection = getattr(algorithm, variables['selection_type'])
-    best, best_eval, best_eval_list, iteration_eval_list = algorithm.algorithm(variables['n_iteration'], variables['r_cross'], variables['r_mutation'], trucks_list, orders_lst, g, selection, variables['uncomplete_sol'])
+    best, best_eval, best_eval_list, iteration_eval_list = algorithm.algorithm(
+        variables['n_iteration'], variables['r_cross'], variables['r_mutation'], trucks_list, orders_lst, g, selection, variables['uncomplete_sol'])
     print("Najlepsze rozwiązanie:")
     for line in best:
         print(line)
@@ -22,8 +35,16 @@ def main():
     Order.reset_id()
     Truck.reset_id()
     algorithm.visualise(best_eval_list, iteration_eval_list)
-    return best, best_eval
-        
-    
+
+    # Reset the stdout to the original
+    sys.stdout = original_stdout
+
+    # Get the output as a string
+    output_string = output_buffer.getvalue()
+
+    # Return the output string
+    return output_string
+
+
 if __name__ == "__main__":
     main()
